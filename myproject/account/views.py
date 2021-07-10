f1d53fd755b14d55c4bc4fe5ec5e799370a7d6a0
tngus000204 <tngus000204@naver.com>
2021-07-10 15:53:11 +0900

    front

--- myproject/account/views.py
+++ myproject/account/views.py
@@ -29,3 +29,26 @@ def signup(request):
             return render(request, 'signup.html', {'error': "비밀번호 확인이 일치하지 않습니다."})
     else:
         return render(request, 'signup.html')
+
+def login(request):
+    if request.method == "POST":
+        id = request.POST['id']
+        password = request.POST['id']
+        user = auth.authenticate(request, username=id, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'login.html', {'error': "사용자 이름 혹은 패스워드가 일치하지 않습니다."})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def main(request):
+    return render(request, 'main.html')
+
+def mypage(request):
+    return render(request, 'mypage.html')
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import account.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', account.views.signup, name = "signup"),
+    path('login/', account.views.login, name = "login"),
+    path('', account.views.main, name = "main"),
+    path('mypage/', account.views.mypage, name = "mypage"),
+    
 ]
